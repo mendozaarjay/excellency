fd6376e4a8fc2c3cfa4840fc2023c5e8f24c3231
Arjay Mendoza <mendozaarjay.05@gmail.com>
2018-08-09 16:06:38 +0800

    Added Company View(Modal)

--- Excellency/Controllers/CompanyController.cs
+++ Excellency/Controllers/CompanyController.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Excellency.Interfaces;
+using Excellency.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Excellency.Controllers
 {
     public class CompanyController : Controller
     {
+        private ICompany _Company;
+
+        public CompanyController(ICompany company)
+        {
+            _Company = company;
+        }
         public IActionResult Index()
         {
-            return View();
+            var result = _Company.Companies().Select
+                (
+                   a => new CompanyViewModel
+                   {
+                       Id = a.Id,
+                       Description = a.Description
+                   }
+                ).ToList();
+            var model = new CompanyIndexViewModel();
+            model.Companies = result;
+            return View(model);
         }
     }
 }
