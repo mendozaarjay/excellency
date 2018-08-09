d52bb9f6475fbb18c12e3c8c37cf149c0837e499
Arjay Mendoza <mendozaarjay.05@gmail.com>
2018-08-09 11:38:57 +0800

    Added RoleModule View Model

---
+++ Excellency/ViewModels/RoleModuleAssignment/RoleModuleHeaderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Excellency.ViewModels
+{
+    public class RoleModuleHeaderViewModel
+    {
+        public int Id { get; set; }
+        public string RoleId { get; set; }
+        public IEnumerable<SelectListItem> Roles { get; set; }
+    }
+}
---
+++ Excellency/ViewModels/RoleModuleAssignment/RoleModuleLineViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Excellency.ViewModels
+{
+    public class RoleModuleLineViewModel
+    {
+        public int Id { get; set; }
+
+        public string RoleModulesHeaderId { get; set; }
+        public string Module { get; set; }
+
+        public bool New { get; set; } 
+        public bool Save { get; set; } 
+        public bool Delete { get; set; } 
+        public bool Search { get; set; } 
+        public bool Print { get; set; } 
+        public bool Post { get; set; } 
+        public bool Import { get; set; } 
+        public bool Export { get; set; } 
+        public bool Approve { get; set; }
+
+
+        public IEnumerable<SelectListItem> Modules { get; set; }
+    }
+}
