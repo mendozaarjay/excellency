d2f1ac1d18789d5a5fb9c4c93babccb1f1f212b4
Arjay Mendoza <mendozaarjay.05@gmail.com>
2018-08-16 09:38:23 +0800

    Added Role and Module Controllers

---
+++ Excellency/Controllers/ModuleController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Excellency.Interfaces;
+using Excellency.ViewModels;
+using Excellency.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Excellency.Controllers
+{
+    public class ModuleController : Controller
+    {
+        private IModule _Module;
+
+        public ModuleController(IModule module)
+        {
+            _Module = module;
+        }
+        public IActionResult Index()
+        {
+            var result = _Module.Modules().Select
+                (a => new ModuleViewModel
+                {
+                    Id = a.Id,
+                    Description = a.Description
+                }).ToList();
+            var model = new ModuleIndexViewModel
+            {
+                Modules = result
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(ModuleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = new Module
+                {
+                    Id = model.Id,
+                    Description = model.Description,
+                };
+                if (model.Id.ToString().Length <= 0)
+                {
+                    _Module.Add(department);
+                }
+                else
+                {
+                    _Module.Update(department);
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _Module.RemoveById(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Excellency/Controllers/RoleController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Excellency.Interfaces;
+using Excellency.Models;
+using Excellency.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Excellency.Controllers
+{
+    public class RoleController : Controller
+    {
+        private IRole _Role;
+
+        public RoleController(IRole role)
+        {
+            _Role = role;
+        }
+        public IActionResult Index()
+        {
+            var result = _Role.Roles().Select
+                (a => new RoleViewModel
+                {
+                    Id = a.Id,
+                    Description = a.Description
+                }).ToList();
+            var model = new RoleIndexViewModel
+            {
+                Roles = result
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = new Role
+                {
+                    Id = model.Id,
+                    Description = model.Description,
+                };
+                if (model.Id.ToString().Length <= 0)
+                {
+                    _Role.Add(department);
+                }
+                else
+                {
+                    _Role.Update(department);
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _Role.RemoveById(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
